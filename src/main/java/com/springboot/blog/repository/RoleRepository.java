9ee5cf8ff78fa335c75a07e4d10a206923bf9225
prasad <prasadpatil2309@gmail.com>
2024-08-06 09:55:09 +0530

    added custom query methods (findByname) to role finding Role table

--- src/main/java/com/springboot/blog/repository/RoleRepository.java
+++ src/main/java/com/springboot/blog/repository/RoleRepository.java
@@ -3,5 +3,9 @@ package com.springboot.blog.repository;
 import com.springboot.blog.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    // finding the role by name
+    Optional<Role> findByName(String name);
 }
