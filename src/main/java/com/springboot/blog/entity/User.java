aaecc2150adca4baa4856d5a576fd6f1e010c309
prasad <prasadpatil2309@gmail.com>
2024-08-07 09:07:50 +0530

    created jpa repo for role and user, created customer user details interface as a part of security authentication

--- src/main/java/com/springboot/blog/entity/User.java
+++ src/main/java/com/springboot/blog/entity/User.java
@@ -33,4 +33,52 @@ public class User {
     inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName = "id")
     )
     private Set<Role> roles;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
